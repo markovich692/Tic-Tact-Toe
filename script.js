a2fdf7b5312ce61cfe29edc6cd5ef28aa3d82a15
Marc Diallo <mark.devguru@outlook.com>
2026-08-24 09:31:24 -0500

    Add gameboard factory and winning move check
    Create a Gameboard factory that stores the board state, players,
    and winning combinations. Track player moves and check whether
    a player has completed any winning combination regardless of move order.

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 "use-strict";
+
+function Gameboard() {
+  const board = ["", "", "", "", "", "", "", "", ""];
+
+  const winningCombination = [
+    [4, 5, 6],
+    [1, 2, 3],
+    [7, 8, 9],
+  ];
+
+  const player1 = { marker: "X", moves: [], score: "" };
+
+  const player2 = { marker: "0", moves: [], score: "" };
+
+  return { player1, player2, board, winningCombination };
+}
+
+const game = Gameboard();
+
+// Player 1 clicks on the 4th square
+
+game.board[4] = game.player1.marker;
+
+game.player1.moves.push(4);
+
+// Player 1 clicks on the 5th square
+
+game.board[5] = game.player1.marker;
+
+game.player1.moves.push(5);
+
+// Player 1 clicks on the 6th square
+
+game.board[6] = game.player1.marker;
+
+game.player1.moves.push(6);
+
+// Checks if the player moves match the winning combination no matter the order
+
+console.log(game.player1.moves);
+
+console.log(game.winningCombination);
+
+const test = game.winningCombination.some(function (combination) {
+  return combination.every(function (square) {
+    return game.player1.moves.includes(square);
+  });
+});
+
+console.log(test);
