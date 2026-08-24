b40c42fe4ed5b948109763b8523b5efea085e60e
Marc Diallo <mark.devguru@outlook.com>
2026-08-24 09:37:35 -0500

    Add all possible winning combinations to the Gameboard

--- script.js
+++ script.js
@@ -4,9 +4,14 @@ function Gameboard() {
   const board = ["", "", "", "", "", "", "", "", ""];
 
   const winningCombination = [
-    [4, 5, 6],
-    [1, 2, 3],
-    [7, 8, 9],
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
   ];
 
   const player1 = { marker: "X", moves: [], score: "" };
